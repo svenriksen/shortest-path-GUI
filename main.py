aa161df10731ee92b996ed021259efe3fe4d7683
Tran Nguyen Sinh <69780117+svenriksen@users.noreply.github.com>
2022-02-22 00:08:09 +0700

    hmm

--- main.py
+++ main.py
@@ -1,34 +1,78 @@
 import tkinter
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
-NavigationToolbar2Tk)
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
-def plot():
-  
-    fig = Figure(figsize = (5, 5), dpi = 100)
-  
-  
-    plot1 = fig.add_subplot(111)
-  
-  
-    canvas = FigureCanvasTkAgg(fig, master = window)  
-    canvas.draw()
-  
-    canvas.get_tk_widget().pack()
-  
-    toolbar = NavigationToolbar2Tk(canvas, window)
-    toolbar.update()
-  
-    canvas.get_tk_widget().pack()
+is_rightclicked = False
+is_leftclicked = False
+
+
+def draw_line_right(event):
+    global is_rightclicked
+    global rightclicked_id
+    x1=event.x-5
+    y1=event.y-5
+    x2=event.x+5
+    y2=event.y+5
+
+    tmppoint = Point(x1, y1)
+    polygon = Polygon(coordinates)
+
+    if polygon.contains(tmppoint):
+        if not is_rightclicked:
+            rightclicked_id = canvas.create_oval(x1,y1,x2,y2,fill="green", outline="")
+            is_rightclicked = True
+
+        else:
+            canvas.delete(rightclicked_id)
+            rightclicked_id = canvas.create_oval(x1,y1,x2,y2,fill="green", outline="")
+        
+
+
+def draw_line_left(event):
+    global is_leftclicked
+    global leftclicked_id
+    x1=event.x-5
+    y1=event.y-5
+    x2=event.x+5
+    y2=event.y+5
+    tmppoint = Point(x1, y1)
+    polygon = Polygon(coordinates)
+
+    if polygon.contains(tmppoint):
+        if not is_leftclicked:
+            
+            leftclicked_id = canvas.create_oval(x1,y1,x2,y2,fill="red", outline="")
+            is_leftclicked = True
+            
+        else:
+            canvas.delete(leftclicked_id)
+            leftclicked_id = canvas.create_oval(x1,y1,x2,y2,fill="red", outline="")
+    
+
 
 if __name__ == '__main__':
     window = tkinter.Tk()
     window.title("Shortest path GUI")
-    window.geometry("1000x600")
+    window.geometry("1500x800")
+
+    canvas = tkinter.Canvas(window, width=1500, height=700)
+    canvas.pack()
+
+    coordinates = []
+
+    global line
+
+    with open("points.txt") as file:
+        line = file.read().splitlines()
+
+    for i in range(len(line)-1):
+        a = line[i].split(" ")
+        b = line[i+1].split(" ")
+        coordinates.append((int(a[0]), int(a[1])))
+        canvas.create_line(int(a[0]), int(a[1]), int(b[0]), int(b[1]), fill="black", width=3)
 
     
-
-    button1 = tkinter.Button(window, text="Click me", width=10, height=1, command = plot)
     
-    button1.pack()
-    window.mainloop()  
+    canvas.bind('<Button-1>', draw_line_left)
+    canvas.bind('<Button-3>', draw_line_right)
+    window.mainloop() 
