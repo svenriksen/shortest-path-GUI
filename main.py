052acfffdc7ff47e95a717fa515064cfdc49490d
Sinh <trannguyensinh2004@gmail.com>
2022-02-22 07:13:51 +0700

    tmp

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
+from re import X
 import tkinter
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
-NavigationToolbar2Tk)
+import numpy
 
-def plot():
-  
-    fig = Figure(figsize = (5, 5), dpi = 100)
-  
-  
-    plot1 = fig.add_subplot(111)
-  
-  
-    canvas = FigureCanvasTkAgg(fig, master = window)  
-    canvas.draw()
-  
-    canvas.get_tk_widget().pack()
-  
-    toolbar = NavigationToolbar2Tk(canvas, window)
-    toolbar.update()
-  
-    canvas.get_tk_widget().pack()
+
+
+class point:
+    X
+    y
 
 if __name__ == '__main__':
     window = tkinter.Tk()
     window.title("Shortest path GUI")
-    window.geometry("1000x600")
+    window.geometry("1500x700")
 
     
+    canvas = tkinter.Canvas(window, width=1500, height=600)
+    canvas.pack()
 
-    button1 = tkinter.Button(window, text="Click me", width=10, height=1, command = plot)
+
+
+    canvas.create_line(100,200,200,35, fill="green", width=4)
+
     
-    button1.pack()
     window.mainloop()  
