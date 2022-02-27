4ce18a93bb4a8e404e6ad7e5d83172a479578a37
Tran Nguyen Sinh <69780117+svenriksen@users.noreply.github.com>
2022-02-27 13:33:02 +0700

    tmp change

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ is_rightclicked = False
 is_leftclicked = False
 graph = {}
 
+n = 0
+m = 0
+startpoint = 0
+endpoint = 0
 
 def create_graph():
     global coordinates
     global graph
+    global n
+    global m
     n = 0
     m = 0
     graph = {}
@@ -36,14 +42,14 @@ def create_graph():
                         graph[i].append(tmp1)
                         canvas.create_line(coordinates[i][0], coordinates[i][1], coordinates[j][0], coordinates[j][1], fill="green", width=1)
 
-
-
 def draw_line_right(event):
+    global startpoint
     global is_rightclicked
     global rightclicked_id
 
     global right_point
-    create_graph()
+    global n
+    global m
 
     x1=event.x-5
     y1=event.y-5
@@ -55,15 +61,19 @@ def draw_line_right(event):
     right_point = tmppoint
     polygon = Polygon(coordinates)
     if polygon.contains(tmppoint):
+        
         if not is_rightclicked:
+            n=n+1
+            startpoint = n-1
             rightclicked_id = canvas.create_oval(x1,y1,x2,y2,fill="green", outline="")
             is_rightclicked = True
 
             for i in range(len(coordinates)-1):
                 path = LineString([coordinates[i], tmppoint])
                 if not path.crosses(polygon):
-                    tmp1 = [i, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
-                    graph[i].append(tmp1)
+                    tmp1 = [n-1, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
+                    graph[n-1].append(tmp1)
+                    m=m+1
 
 
         else:
@@ -74,16 +84,20 @@ def draw_line_right(event):
                 path = LineString([coordinates[i], tmppoint])
 
                 if not path.crosses(polygon):
-                    tmp1 = [i, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
-                    graph[i].append(tmp1)              
+                    tmp1 = [n-1, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
+                    graph[n-1].append(tmp1)  
+                    m=m+1            
     
 
 def draw_line_left(event):
+    global ebdpoint
     global is_leftclicked
     global leftclicked_id
     
     global left_point
-    create_graph()
+    global n
+    global m
+
     x1=event.x-5
     y1=event.y-5
     x2=event.x+5
@@ -98,14 +112,16 @@ def draw_line_left(event):
 
         if not is_leftclicked:
             
+            n=n+1
             leftclicked_id = canvas.create_oval(x1,y1,x2,y2,fill="red", outline="")
             is_leftclicked = True
 
             for i in range(len(coordinates)-1):
                 path = LineString([coordinates[i], tmppoint])
                 if not path.crosses(polygon):
-                    tmp1 = [i, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
-                    graph[i].append(tmp1)
+                    tmp1 = [101, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
+                    graph[101].append(tmp1)
+                    m=m+1
             
         else:
             canvas.delete(leftclicked_id)
@@ -114,15 +130,16 @@ def draw_line_left(event):
                 path = LineString([coordinates[i], tmppoint])
 
                 if not path.crosses(polygon):
-                    tmp1 = [i, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
-                    graph[i].append(tmp1)
+                    tmp1 = [101, math.sqrt((coordinates[i][0]-tmppoint.x)**2 + (coordinates[i][1]-tmppoint.y)**2)]
+                    graph[101].append(tmp1)
+                    m=m+1
 
     
 def dijkstra():
     if (is_leftclicked == True and is_rightclicked == True):
         print(right_point)
         print(left_point)
-        print("Dijkstra")   
+        print("Dijkstra")
 
 if __name__ == '__main__':
     window = tkinter.Tk()
